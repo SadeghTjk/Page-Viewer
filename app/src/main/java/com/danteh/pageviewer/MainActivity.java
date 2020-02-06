53c990743357c99d61a15849df3ba4881dc9d42d
Shadow <42289385+sadeghgaga@users.noreply.github.com>
2020-02-06 13:29:02 +0330

    if added

--- app/src/main/java/com/danteh/pageviewer/MainActivity.java
+++ app/src/main/java/com/danteh/pageviewer/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     WebView webView;
     EditText editText;
+    int i=0;
+
     public final static String TAG = "webview";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +22,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.wv);
         editText = findViewById(R.id.et);
-
+        final String[] urls = {"https://www.all.ir/","https://www.all.ir/%d8%b3%d8%a7%d9%86%d8%af%d8%a8%d8%a7%d8%b1-%d8%b3%d8%a7%d9%85%d8%b3%d9%88%d9%86%da%af-hw-j7591/","https://www.google.com/"};
         webView.getSettings().setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new WebViewClient() {
 
             public void onPageFinished(WebView view, String url) {
                 Log.e(TAG, "onPageFinished: LOADED" );
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this,"یک امتیاز اضافه شد!",Toast.LENGTH_LONG).show();
-                    }
-                },5000);
-            }
-        });
 
-        webView.loadUrl("https://www.all.ir/");
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Toast.makeText(MainActivity.this,"یک امتیاز اضافه شد!",Toast.LENGTH_SHORT).show();
+                            if(i<urls.length){
+                                webView.loadUrl(urls[i]);
+                                i++;
+                            }
+                        }
+
+                    },5000);
+                }
+            });
+        webView.loadUrl(urls[i]);
+        i++;
 
     }
 }
